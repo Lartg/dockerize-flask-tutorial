3e3e624d4aa545f17d0afe84b3ec5d2f38036640
Lartg <roc.gerrety@gmail.com>
2021-11-01 10:45:52 -0800

    added bootstrap, navbar is work in progress

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def coffee_index():
-    return render_template('coffee_index.html')
+  return render_template('coffee_index.html')
+
+
+@app.route('/coffee/recipes')
+def coffee_recipes():
+  return render_template('coffee_recipes.html')
+
+
+@app.route('/coffee/variables')
+def coffee_vars():
+  return render_template('coffee_vars.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
