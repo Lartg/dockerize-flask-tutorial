db2c8bc97fbd19f4499a44c6a1c4cdedccc06d2d
Lartg <roc.gerrety@gmail.com>
2021-11-01 09:52:05 -0800

    added flask and navbar template

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def coffee_index():
+    return render_template('coffee_index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
